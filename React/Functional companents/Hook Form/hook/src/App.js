7666b8ac2861b73bbd58970a05f99bf337a58f23
jehad-issa <96985283+jehad-issa@users.noreply.github.com>
2022-04-11 23:12:22 +0300

    hook

---
+++ React/Functional companents/Hook Form/hook/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UserForm from './components/myComponents';
+
+function App() {
+  return (
+    <div className="App">
+        <UserForm/>
+    </div>
+  );
+}
+
+export default App;
