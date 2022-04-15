1eb81c48c810b7231892ffbdb66b2e9b37f10910
jehad-issa <96985283+jehad-issa@users.noreply.github.com>
2022-04-15 22:16:08 +0300

    pokemon

---
+++ React/APIs/Pokemon API/pokemon/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Pokemon from './components/ApiPokemon';
+
+function App() {
+  return (
+    <div className="App">
+      <Pokemon/>
+    </div>
+  );
+}
+
+export default App;
